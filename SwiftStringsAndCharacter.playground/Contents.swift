a7941cbc73a7f79636122836799624bc055c6688
bhaskerrai <brai285@gmail.com>
2022-04-13 22:54:43 +0500

    new

--- SwiftStringsAndCharacter.playground/Contents.swift
+++ SwiftStringsAndCharacter.playground/Contents.swift
@@ -1,9 +1,43 @@
 import UIKit
 
-var chap = "It's chapter 3"
-print(chap)
+/*A string literal is a sequence of characters surrounded by double quotation marks (").
+ 
+Multiline String Literals
+If you need a string that spans several lines, use a multiline string literal—a sequence of characters surrounded by three double quotation marks.
+ */
 
-//So where was I?
+let paragaph = """
+This is first line
+this is second
+this is third
+"""
 
-let ab = "Strings are very important"
-print(ab)
+print(paragaph)
+
+let multilineString = """
+“The White Rabbit put on his spectacles.  "Where shall I begin, \
+please your Majesty?" he asked.
+
+"Begin at the beginning," the King said gravely, "and go on \
+till you come to the end; then stop.”
+"""
+print(multilineString)
+
+/*Special Characters in String Literals
+String literals can include the following special characters:
+
+1.The escaped special characters \0 (null character), \\ (backslash), \t (horizontal tab), \n (line feed), \r (carriage return), \" (double quotation mark) and \' (single quotation mark)”
+2.An arbitrary Unicode scalar value, written as \u{n}, where n is a 1–8 digit hexadecimal number (Unicode is discussed in Unicode below)”
+*/
+
+let dollarSign = "\u{24}"        // $,  Unicode scalar U+0024
+let blackHeart = "\u{2665}"      // ♥,  Unicode scalar U+2665
+let sparklingHeart = "\u{1F496}" // 💖, Unicode scalar U+1F496”
+
+print(dollarSign,blackHeart,sparklingHeart)
+
+let threeDoubleQuotationMarks = """
+Escaping the first quotation mark \"""
+Escaping all three quotation marks \"""
+"""
+
